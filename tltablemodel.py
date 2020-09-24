1e7d50c95b2591eb47dcb91344a145cd3e5faa96
Efarov Ilya <efarov.ily@mail.ru>
2020-09-24 11:02:09 +0900

    add domain name and service name resolving

--- tltablemodel.py
+++ tltablemodel.py
@@ -6,6 +6,11 @@ from PySide2.QtCore import QAbstractTableModel, Qt, QModelIndex, Slot
 from PySide2.QtGui import QColor
 
 
+
+
+def isZeroIPAddress(addr: bytes, family: socket.AddressFamily)-> bool:
+    return addr == (bytes([0] * 4) if family != socket.AF_INET6 else bytes([0] * 16))
+
 def nameTransportProtocol(family: socket.AddressFamily, type: socket.SocketKind)-> str:
     """ Return str representation name transport protocol """
     if type == socket.SOCK_STREAM:
@@ -24,19 +29,17 @@ def psutilAddrToIPAndPort(paddr, pfamily)-> tuple:      # -> tuple(bytes, int)
         ip, port = ('::', 0) if pfamily == socket.AF_INET6 else ('0.0.0.0', 0)
     return socket.inet_pton(pfamily, ip), port
 
+def ipToDomainName(addr: str)-> str:
+    try:
+        return socket.gethostbyaddr(addr)[0]
+    except (socket.herror, socket.gaierror):
+        return addr
 
-def IPToViewStr(ip: bytes, family: socket.AddressFamily, type: socket.SocketKind)-> str:
-    zero_ip = bytes([0]*4) if family != socket.AF_INET6 else bytes([0]*16)
-    if ip == zero_ip:
-        if type == socket.SOCK_DGRAM:
-            return "*"
-    return socket.inet_ntop(family, ip)
-
-def portToViewStr(port: int, type: socket.SocketKind)-> str:
-    if port == 0:
-        if type == socket.SOCK_DGRAM:
-            return "*"
-    return str(port)
+def portToServiceName(port: int, type: socket.SocketKind)-> str:
+    try:
+        return socket.getservbyport(port, 'tcp' if type == socket.SOCK_STREAM else 'udp')
+    except OSError:
+        return str(port)
 
 def statusToViewStr(val: str)-> str:
     return '' if val == "NONE" else val
@@ -53,6 +56,17 @@ def generateCountValueInTable(tltablemodel, column, value):
     return countValueInTable
 
 
+def psutilConnectionToList(connection: psutil._common.sconn)-> list:
+    """ transfer psutil._common.sconn to list """
+    try:
+        process = psutil.Process(connection.pid)
+        return [process.name(), connection.pid, (connection.family, connection.type),
+               *psutilAddrToIPAndPort(connection.laddr, connection.family),
+               *psutilAddrToIPAndPort(connection.raddr, connection.family), connection.status]
+    except psutil.NoSuchProcess:
+        return []
+
+
 # class TLTableModel is model table for work with host's network connections on transport layer
 class TLTableModel(QAbstractTableModel):
     # All main headers in TLTableModel
@@ -63,6 +77,9 @@ class TLTableModel(QAbstractTableModel):
     def __init__(self):
         super().__init__()
         self.net_connections = []   # main model table
+        self.domainNameMode = False # return numeric address or domain name?
+        # self.cacheDomainNames = {}
+        self.serviceNameMode = True # return numeric port  or service name?
         self.sortColumn = 0         # sorted column number
         self.sortASC = False        # ascending sort?
         # create function for count the number of rows status == STATUS_VALUES[1]
@@ -73,27 +90,70 @@ class TLTableModel(QAbstractTableModel):
         # load system data in self.net_connections
         self.updateData()
 
-
     @Slot()
     def updateData(self):
         """ load system data about all network connections on taransport layer and
             create data table """
         # notify the view of the begin of a radical change in data
         self.beginResetModel()
-        self.net_connections = []
+        net_connections = []
         # create data table
         for connection in psutil.net_connections():
-            try:
-                process = psutil.Process(connection.pid)
-            except psutil.NoSuchProcess:
-                continue
-            row = [process.name(), connection.pid, (connection.family, connection.type),
-                   *psutilAddrToIPAndPort(connection.laddr, connection.family),
-                   *psutilAddrToIPAndPort(connection.raddr, connection.family), connection.status]
-            self.net_connections.append(row)
+            row = psutilConnectionToList(connection)
+            if len(row) > 0:
+                net_connections.append(row)
+
+        # def listsIsDifferent(list1, list2, *indexes):
+        #     """ The function checks the differences of lists at the specified indices. """
+        #     if len(list1) != len(list2):
+        #         raise ValueError("Lists should have same length!!!")
+        #     count = 0
+        #     for index in indexes:
+        #         if list1[index] != list2[index]:
+        #             count += 1
+        #     return count > 0
+
+        # def tablesDifference(table1: list, table2: list, *columns)-> list:
+        #     """ The function searches for rows of the first table that are not in the second table. """
+        #     answer = []
+        #     for i in range(len(table1)):
+        #         count = 0
+        #         for j in range(len(table2)):
+        #             if listsIsDifferent(table1[i], table2[j], *columns):
+        #                 count += 1
+        #         if count == len(table2):
+        #             answer.append(table1[i])
+        #     return answer
+
+        # def updatedRows(old_table: list, new_table: list, indexes_id: tuple, indexes_cmp: tuple)-> list:
+        #     """ The function сhecks the rows of the old table that are updated in the new table.
+        #         * old_table - old table; new_table - new table;
+        #           indexes_id - indexes identifying a row in a table;
+        #           indexes_cmp - data indexes in which to compare. These are the indices of the elements
+        #           for which the data could be updated;"""
+        #     answer = []
+        #     for i in range(len(old_table)):
+        #         for j in range(len(new_table)):
+        #             if not listsIsDifferent(old_table[i], new_table[j], *indexes_id) and \
+        #                     listsIsDifferent(old_table[i], new_table[j], *indexes_cmp):
+        #                 answer.append(old_table[i])
+        #
+        # deleted = tablesDifference(self.net_connections, net_connections, 2, 3, 4, 5, 6)
+        # created = tablesDifference(net_connections, self.net_connections, 2, 3, 4, 5, 6)
+        # updated = updatedRows(self.net_connections, net_connections, (1, 2, 3, 4, 5, 6), (7,))
+
+        self.net_connections = net_connections
         self.sortData()
         # notify the view of the end of a radical change in data
         self.endResetModel()
+
+    def setDomainNameMode(self, flag: bool):
+        """ numeric address or domain name? """
+        self.domainNameMode = flag
+
+    def setServiceNameMode(self, flag: bool):
+        """ numeric port or service name? """
+        self.serviceNameMode = flag
 
     @Slot()
     def sortData(self):
@@ -138,9 +198,22 @@ class TLTableModel(QAbstractTableModel):
                 if column == 2:
                     return nameTransportProtocol(*self.net_connections[row][column])
                 elif column == 3 or column == 5:
-                    return IPToViewStr(self.net_connections[row][column], *self.net_connections[row][2])
-                elif column == 6:
-                    return portToViewStr(self.net_connections[row][column], self.net_connections[row][2][1])
+                    if column == 5:
+                        if isZeroIPAddress(self.net_connections[row][column], self.net_connections[row][2][0]) and \
+                                self.net_connections[row][2][1] == socket.SOCK_DGRAM:
+                            return '*'
+                    addr = socket.inet_ntop(self.net_connections[row][2][0], self.net_connections[row][column])
+                    if self.domainNameMode:
+                        return ipToDomainName(addr)
+                    return addr
+                elif column == 4 or column == 6:
+                    if column == 6:
+                        if self.net_connections[row][column] == 0 and \
+                                self.net_connections[row][2][1] == socket.SOCK_DGRAM:
+                            return '*'
+                    if self.serviceNameMode:
+                        return portToServiceName(self.net_connections[row][column], self.net_connections[row][2][1])
+                    return str(self.net_connections[row][column])
                 elif column == 7:
                     return statusToViewStr(self.net_connections[row][column])
                 return str(self.net_connections[row][column])
