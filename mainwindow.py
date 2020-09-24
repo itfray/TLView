6e6002aa6bd41aeb6bf7f2b79bcf4eb1d3b294bd
Efarov Ilya <efarov.ily@mail.ru>
2020-09-24 09:31:04 +0900

    link updating of table model with timer

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,10 @@
 from PySide2.QtWidgets import QMainWindow
 from ui_mainwindow import Ui_MainWindow
 from TLTableModel import TLTableModel
+from PySide2.QtCore import QTimer
+
+
+TIMER_VALUES = (1000, 3000, 5000)
 
 
 class MainWindow(QMainWindow):
@@ -8,6 +12,13 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
         # link model with view
         self.tlTableModel = TLTableModel()
         self.ui.tableView.setModel(self.tlTableModel)
+
+        # link model with timer
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.tlTableModel.updateData)
+        self.timer.setInterval(TIMER_VALUES[0])
+        self.timer.start()
